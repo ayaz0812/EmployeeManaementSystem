5076afeebea9cb5f3298618bbb8d4d6fdeeb5a08
Ayaz Ahmad <132739334+ayaz0812@users.noreply.github.com>
2024-03-30 22:40:57 +0530

    Final commit

---
+++ src/employeemanagementsystem/db.sql
@@ -0,0 +1,5 @@
+SELECT * FROM employeemanagementsystem.employee;
+create table employee(
+name varchar(255),fname varchar(20), dob varchar(30) ,salary varchar(40),phone varchar(10), desgination varchar(25));
+select * from employee;
+insert into employee values("test","test","bfdh",100,1234567890,"dhsh");
